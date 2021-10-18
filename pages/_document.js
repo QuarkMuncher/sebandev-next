badc78514f7519b0fa10ba06a9dd7587e6bb90ba
Sebastian Andersen <sebastian@sandersen.co>
2021-10-18 15:40:11 +0200

    Shot in the dark

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,7 @@ export default class MyDocument extends Document {
     return(
       <Html>
         <Head>
-          <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
+          <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css?v=1234"
                 integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm"
                 crossOrigin="anonymous" />
         </Head>
