c71148bfc64390ed555126ae649e9d3a7954bc60
Sebastian Andersen <sebastian@sandersen.co>
2021-10-20 16:53:14 +0200

    More attemtpts to fix the blogpage header error on iphone

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -36,7 +36,7 @@ export default function Index({postData}) {
         <ul className='flex flex-col gap-2 mt-3'>
           {postData.map(post => (
             <li key={post.slug}>
-              <InternalLink href={`/blog/post/${post.slug}`}>{<SmallTitle text={post.title} />}</InternalLink>
+              <InternalLink href={`/blog/post/${post.slug}`}>{<SmallTitle className={`inline-block`} text={post.title} />}</InternalLink>
               <SubTitle text={post.subTitle} />
               <Author name={post.author} />
               <small>
