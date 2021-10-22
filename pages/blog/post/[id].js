19558fb9fe083a603482f061896e263d948c1650
Sebastian Andersen <sebastian@sandersen.co>
2021-10-22 16:03:24 +0200

    Fixing wrong itemprop attribute

--- pages/blog/post/[id].js
+++ pages/blog/post/[id].js
@@ -28,8 +28,8 @@ export default function Post({postData}) {
       <Head>
         <title>{postData.title} | Blog | Seban.dev</title>
         <meta name="description" content={postData.description} />
-        <meta itemProp="name" content={`${postData.title} | seban.dev`} />
-        <meta itemProp="description" content={postData.description} />
+        <meta itemprop="name" content={`${postData.title} | seban.dev`} />
+        <meta itemprop="description" content={postData.description} />
       </Head>
       <NavBar links={[
         {
