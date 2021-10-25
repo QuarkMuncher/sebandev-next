5365bc2c87c52f154aa25184d94241527d7e00b0
Sebastian Andersen <sebastian@sandersen.co>
2021-10-25 09:08:15 +0200

    Adds lang attribute to HTML tag

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,7 @@ export default class MyDocument extends Document {
 
   render() {
     return(
-      <Html>
+      <Html lang={`eng`} >
         <Head>
           {/*<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css?v=1234"
                 integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm"
